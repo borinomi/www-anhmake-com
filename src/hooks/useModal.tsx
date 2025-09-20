a82e8978b39259c341f0a77eeef496a2515ec73a
Hotae Kim <hotaekim@Hotaeui-MacBookPro.local>
2025-09-20 18:15:37 +0900

    section3

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -190,7 +190,7 @@ export const useModal = (options: UseModalOptions = {}) => {
                     <select id="cardSection" name="cardSection" className="form-select" defaultValue={modalData?.sectionId || ''}>
                       {options.availableSections?.map(section => (
                         <option key={section.id} value={section.id}>
-                          {section.dashboard_title} > {section.title}
+                          {section.dashboard_title} → {section.title}
                         </option>
                       ))}
                     </select>
